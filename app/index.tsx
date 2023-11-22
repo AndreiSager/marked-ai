6b9ff5c025c0a6c5960eae67a472691b43ceedb9
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-11-22 09:32:41 +0800

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, SIZES, icons, images } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+        }}
+      />
       <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   );
 }
