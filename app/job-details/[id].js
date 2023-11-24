b3a77bcc15a19383ab3de581d117883eede79afe
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-11-24 15:14:49 +0800

    Job Details page complete

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -114,6 +114,13 @@ const JobDetails = () => {
             </View>
           )}
         </ScrollView>
+
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results"
+          }
+        />
       </>
     </SafeAreaView>
   );
